3c4a61221b6600c9d5a061aaeca2f7406e8525d2
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-04-15 16:33:19 +0600

    completed my_each_with_index

--- methods_enumerable.rb
+++ methods_enumerable.rb
@@ -1,11 +1,26 @@
 module Enumerable
   def my_each
-    for i in 0...size
-      yield self[i]
+    for item in self
+      yield item
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    for item in self
+      yield(item, i)
+      i += 1
     end
   end
 end
+
 arr=[3,4,6,8,7]
 hash={a: 1, b: 2}
-arr.my_each{|item| puts item.to_s}
-hash.each{|item| puts item.to_s}
+
+# TEST
+
+#arr.my_each{|item| puts item.to_s}
+#hash.my_each{|item| puts item.to_s}
+
+#arr.my_each_with_index { |v, i| puts "#{v} at index #{i}" }
+#hash.my_each_with_index { |k, v, i| puts "#{k}: #{v} at index #{i}" }
